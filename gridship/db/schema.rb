f80e1528ca141de3f27f75b429db25e241041651
David Tisdall <david.tisdall@thursday5th.com.au>
2015-09-19 00:44:41 +1000

    Messing around with rails

--- gridship/db/schema.rb
+++ gridship/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914233116) do
+ActiveRecord::Schema.define(version: 20150915135934) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "jumbo_juice"
   end
 
 end
